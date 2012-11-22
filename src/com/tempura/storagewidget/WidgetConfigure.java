9c3844465c1d02fbf4982dcaa3a34e69dd34c5df
chunmeng <chunmeng@yahoo.com>
2012-11-22 23:22:27 +0800

    Resolves application not installed on phone error

--- src/com/tempura/storagewidget/WidgetConfigure.java
+++ src/com/tempura/storagewidget/WidgetConfigure.java
@@ -9,7 +9,7 @@ public class WidgetConfigure extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_configure);
+        setContentView(R.layout.widget_configure);
     }
 
     @Override
