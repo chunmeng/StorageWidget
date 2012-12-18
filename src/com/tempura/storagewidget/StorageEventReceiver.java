269691a6ec13711a0a71fa102ff38c894b371fd8
chunmeng <oceanuz@gmail.com>
2012-12-19 00:20:38 +0800

    Goodnite checkpoint again. Timer seems to work. Need more test

--- src/com/tempura/storagewidget/StorageEventReceiver.java
+++ src/com/tempura/storagewidget/StorageEventReceiver.java
@@ -1,5 +1,8 @@
 package com.tempura.storagewidget;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -9,7 +12,27 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class StorageEventReceiver extends BroadcastReceiver {
-    private final String TAG = "StorageWidget:Receiver";
+    private final String TAG = "StorageWidget:Receiver";  
+    
+    class MyTimerTask extends TimerTask {
+        private AppWidgetManager mMgr;
+        private ComponentName mComp;
+        
+        public MyTimerTask(AppWidgetManager mgr, ComponentName comp) {
+        	super();
+        	this.mMgr = mgr;        
+        	this.mComp = comp;
+        }
+    	
+    	@Override
+    	public void run()    
+        {            
+    		if (this.mMgr != null) {
+                int[] arrayOfId = mMgr.getAppWidgetIds(this.mComp);
+                mMgr.notifyAppWidgetViewDataChanged(arrayOfId, R.id.storage_list);    			
+    		}   			
+        }
+    }    
     
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -26,17 +49,27 @@ public class StorageEventReceiver extends BroadcastReceiver {
             int[] arrayOfId = mgr.getAppWidgetIds(new ComponentName(context.getPackageName(), BetterStorageWidgetProvider.class.getName()));
             mgr.notifyAppWidgetViewDataChanged(arrayOfId, R.id.storage_list);
             
-        } else if (action.equals(Intent.ACTION_MEDIA_REMOVED) 
-                    || action.equals(Intent.ACTION_MEDIA_UNMOUNTED)) {
+        } else if (action.equals(Intent.ACTION_MEDIA_REMOVED)) {
             Toast.makeText(context, intent.getDataString().replace("file://", "") + " removed...", Toast.LENGTH_SHORT).show();
             
-            // TODO: Need to delayed the data changed notify for 1-2s. 
-            // It seems the unmount will take sometime to reflect on the df table, and the external sd still appear. 
         	// Trigger widget update - which should just rescan mounts
             final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
             int[] arrayOfId = mgr.getAppWidgetIds(new ComponentName(context.getPackageName(), BetterStorageWidgetProvider.class.getName()));            
             mgr.notifyAppWidgetViewDataChanged(arrayOfId, R.id.storage_list);
-        }
+        } else if (action.equals(Intent.ACTION_MEDIA_UNMOUNTED)) {
+        	Toast.makeText(context, intent.getDataString().replace("file://", "") + " unmounted...", Toast.LENGTH_SHORT).show();
+        
+        	// TODO: Need to delayed the data changed notify for 1-2s. 
+	        // It seems the unmount will take sometime to reflect on the df table, and the external sd still appear. 
+	    	// Trigger widget update - which should just rescan mounts
+	        final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+	        // int[] arrayOfId = mgr.getAppWidgetIds(new ComponentName(context.getPackageName(), BetterStorageWidgetProvider.class.getName()));            
+	        // mgr.notifyAppWidgetViewDataChanged(arrayOfId, R.id.storage_list);
+	        
+	        MyTimerTask task = new MyTimerTask(mgr, new ComponentName(context.getPackageName(), BetterStorageWidgetProvider.class.getName())); //create new task
+	        Timer timer = new Timer(); 
+	        timer.schedule(task,2*1000L); // start a new timer task in 5seconds (timertask, seconds(long))
+        }        
         
     }
 }
